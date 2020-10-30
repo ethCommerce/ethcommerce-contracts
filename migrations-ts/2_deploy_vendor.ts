2a9a1fff2a27992de29480d625c257de4fe005af
Maximiliaan van Dijk <max@maxilibrium.com>
2020-10-29 23:26:58 -0600

    simplified migrations

--- migrations-ts/2_deploy_vendor.ts
+++ migrations-ts/2_deploy_vendor.ts
@@ -4,12 +4,10 @@ const VendorProduct = artifacts.require('VendorProduct')
 const Vendor = artifacts.require('Vendor')
 
 module.exports = async function (deployer) {
-    const vendorAccessControl = await deployer.deploy(VendorAccessControl);
-    const vendorData = await deployer.deploy(VendorData)
-    const vendor = await deployer.deploy(Vendor)
-    const vendorProduct = await deployer.deploy(VendorProduct)
-
-    console.log(vendorAccessControl)
+    await deployer.deploy(VendorAccessControl);
+    await deployer.deploy(VendorData)
+    await deployer.deploy(Vendor)
+    await deployer.deploy(VendorProduct)
 
 } as Truffle.Migration
 
