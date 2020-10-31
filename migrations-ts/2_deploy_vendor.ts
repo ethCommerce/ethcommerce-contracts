acb9befcf1f8fb89cdb1586a8761ac73645f16fd
Maximiliaan van Dijk <max@maxilibrium.com>
2020-10-30 20:12:03 -0600

    Updated migrations file

--- migrations-ts/2_deploy_vendor.ts
+++ migrations-ts/2_deploy_vendor.ts
@@ -1,13 +1,15 @@
 const VendorAccessControl = artifacts.require('VendorAccessControl')
 const VendorData = artifacts.require('VendorData')
 const VendorProduct = artifacts.require('VendorProduct')
+const VendorInvoice = artifacts.require('VendorInvoice')
 const Vendor = artifacts.require('Vendor')
 
 module.exports = async function (deployer) {
-    await deployer.deploy(VendorAccessControl);
+    await deployer.deploy(VendorAccessControl)
     await deployer.deploy(VendorData)
-    await deployer.deploy(Vendor)
     await deployer.deploy(VendorProduct)
+    await deployer.deploy(VendorInvoice)
+    await deployer.deploy(Vendor)
 
 } as Truffle.Migration
 
